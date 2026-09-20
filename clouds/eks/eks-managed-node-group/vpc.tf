2eb080f5c9c1a79a8ee95e83613d3f757d32f88b
Ming H <ming.ddtechcg@gmail.com>
2026-09-20 19:27:20 -0400

    restructured

---
+++ clouds/eks/eks-managed-node-group/vpc.tf
@@ -0,0 +1,21 @@
+# VPC
+# https://registry.terraform.io/modules/terraform-aws-modules/vpc/aws/latest
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "6.7.2"
+
+  name = "${local.name}-vpc"
+  cidr = local.vpc_cidr
+
+  azs             = local.azs
+  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)]
+  public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 48)]
+  intra_subnets   = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 52)]
+
+  enable_nat_gateway     = true
+  single_nat_gateway     = false
+  one_nat_gateway_per_az = false
+
+
+  tags = local.tags
+}
