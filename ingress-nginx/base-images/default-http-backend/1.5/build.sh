00ae4b12f0a6919b8f0a9013bda9867f42dca08e
Ming H <ming.ddtechcg@gmail.com>
2026-09-22 21:42:43 -0400

    restructured

--- ingress-nginx/base-images/default-http-backend/1.5/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-docker build -f Dockerfile -t harbor.ddtechcg.com:5001/k8s-utils/defaultbackend-amd64:1.5 .
-
--- ingress-nginx/base-images/ingress-nginx/v1.11.2/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-docker build -f Dockerfile -t harbor.ddtechcg.com:5001/k8s-utils/ingress-nginx-controller:v1.11.2 .
-
